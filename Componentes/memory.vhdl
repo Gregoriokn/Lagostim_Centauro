4b8ea7aefb0f63c43f561e09c75008043d14a268
Eduardo Valim <edvalim@icloud.com>
2022-10-10 13:54:50 -0400

    pequenas alteracoes na mem

--- Componentes/memory.vhdl
+++ Componentes/memory.vhdl
@@ -3,7 +3,6 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
-
 entity memory is
     generic (
         addr_width: natural := 16; -- Memory Address Width (in bits)
@@ -12,19 +11,18 @@ entity memory is
     port (
         clock: in std_logic; -- Clock signal; Write on Falling-Edge
 
-        data_read : in std_logic; -- When '1', read data from memory
+        data_read: in std_logic; -- When '1', read data from memory
         data_write: in std_logic;-- When '1', write data to memory
         -- Data address given to memory
-        data_addr : in std_logic_vector(addr_width-1 downto 0);
+        data_addr: in std_logic_vector(addr_width-1 downto 0);
         -- Data sent from memory when data_read = '1' and data_write = '0'
-        data_in : in std_logic_vector(data_width-1 downto 0);
+        data_in: in std_logic_vector(data_width-1 downto 0);
         -- Data sent to memory when data_read = '0' and data_write = '1'
-        data_out : out std_logic_vector((data_width*4)-1 downto 0)
+        data_out  out std_logic_vector((data_width*4)-1 downto 0)
         );
 end entity;
 
 architecture behavioral of memory is 
-
 --cria matriz de 2^16 pos com cada uma de tamanho 8 bits--
 type mem_type is array (0 to 2**addr_width) of std_logic_vector(data_width-1 downto 0);
 signal mem: mem_type
