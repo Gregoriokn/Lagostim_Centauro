5fcb3087657cf69ec69a25bd20161f07accf7234
Gregório Koslinski <gregoriokoslinskineto@gmail.com>
2022-10-13 21:18:46 -0400

    codec test

--- Componentes/codec.vhdl
+++ Componentes/codec.vhdl
@@ -17,3 +17,30 @@ entity codec is
         codec_data_out : out std_logic_vector(7 downto 0)
 );
 end entity;
+
+begin 
+
+    escrita : process
+        type t_arq_out is file of std_logic_vector(7 downto 0);
+        file arq_dados_out : t_arq_out
+            open append_mode is "dados_out.dat";
+    begin
+        wait on interrupt until write_signal = '1';
+        write(arq_dados_out, codec_data_out);
+
+        wait;
+    end process;
+
+    leitura : process 
+     type t_arq_in is file of std_logic_vector(7 downto 0);
+     file arq_pacote : t_arq_in 
+        open read_mode is "dados_in.dat";
+
+    begin
+        wait on interrupt until read_signal = '1';
+        read(arq_pacote,codec_data_in);
+
+    end process;
+
+
+end
