9b047b5fe4b78aeca12ad1a5ad9500c2f7703254
Eduardo Valim <eduardovalim@Valims-MacBook-Pro.local>
2022-11-13 16:07:52 -0400

    Nova estrutura da CPU

--- Componentes/cpu.vhdl
+++ Componentes/cpu.vhdl
@@ -23,7 +23,7 @@ entity cpu is
         -- Data address given to memory
         mem_data_addr : out std_logic_vector(addr_width-1 downto 0);
         -- Data sent to memory when mem_data_read = '0' and mem_data_write = '1' (comentário corrigido)
-        mem_data_in : out std_logic_vector((data_width*4)-1 downto 0);
+        mem_data_in : out std_logic_vector((data_width*8)-1 downto 0);
         -- Data sent from memory when mem_data_read = '1' and mem_data_write = '0' (comentário corrigido)
         mem_data_out : in std_logic_vector(data_width-1 downto 0);
         ---- End Memory Signals ---
@@ -43,110 +43,72 @@ entity cpu is
 end entity;    
 
 architecture behavioral of cpu is 
-constant HLT      : std_logic_vector(3 DOWNTO 0):="0000"; --Interrompe execucao indefinidamente.--
-constant IN_      : std_logic_vector(3 DOWNTO 0):="0001"; --Empilha um byte recebido do codec.--   
-constant OUT_     : std_logic_vector(3 DOWNTO 0):="0010"; --Desempilha um byte e o envia para o codec.
-constant PUSHIP   : std_logic_vector(3 DOWNTO 0):="0011"; --Empilha o endereço armazenado no registrador IP(2 bytes, primeiro MSB e depois LSB).
-constant PUSH_IMM : std_logic_vector(3 DOWNTO 0):="0100"; --Empilha um byte contendo imediato (armazenado nos 4 bits menos significativos da instru¸c˜ao).
-constant DROP     : std_logic_vector(3 DOWNTO 0):="0101"; --Elimina um elemento da pilha
-constant DUP      : std_logic_vector(3 DOWNTO 0):="0110"; --Reempilha o elemento no topo da pilha
-constant ADD      : std_logic_vector(3 DOWNTO 0):="1000"; --Desempilha Op1 e Op2 e empilha (Op1 + Op2).
-constant SUB      : std_logic_vector(3 DOWNTO 0):="1001"; --Desempilha Op1 e Op2 e empilha (Op1 − Op2).
-constant NAND     : std_logic_vector(3 DOWNTO 0):="1010"; --Desempilha Op1 e Op2 e empilha NAND(Op1, Op2).
-constant SLT      : std_logic_vector(3 DOWNTO 0):="1011"; --Desempilha Op1 e Op2 e empilha (Op1 < Op2).
-constant SHL      : std_logic_vector(3 DOWNTO 0):="1100"; --Desempilha Op1 e Op2 e empilha (Op1 ≪ Op2).
-constant SHR      : std_logic_vector(3 DOWNTO 0):="1101"; --Desempilha Op1 e Op2 e empilha (Op1 ≫ Op2).
-constant JEQ      : std_logic_vector(3 DOWNTO 0):="1110"; --Desempilha Op1(1 byte), Op2(1 byte) e Op3(2 bytes); Verifica se (Op1 = Op2), caso positivo soma Op3 no registrador IP.
-constant JMP      : std_logic_vector(3 DOWNTO 0):="1111"; --Desempilha Op1(2 bytes) e o atribui no registrador IP
+constant op_HLT      : std_logic_vector(3 DOWNTO 0):="0000"; --Interrompe execucao indefinidamente.--
+constant op_IN       : std_logic_vector(3 DOWNTO 0):="0001"; --Empilha um byte recebido do codec.--   
+constant op_OUT      : std_logic_vector(3 DOWNTO 0):="0010"; --Desempilha um byte e o envia para o codec.
+constant op_PUSHIP   : std_logic_vector(3 DOWNTO 0):="0011"; --Empilha o endereço armazenado no registrador IP(2 bytes, primeiro MSB e depois LSB).
+constant op_PUSH_IMM : std_logic_vector(3 DOWNTO 0):="0100"; --Empilha um byte contendo imediato (armazenado nos 4 bits menos significativos da instru¸c˜ao).
+constant op_DROP     : std_logic_vector(3 DOWNTO 0):="0101"; --Elimina um elemento da pilha
+constant op_DUP      : std_logic_vector(3 DOWNTO 0):="0110"; --Reempilha o elemento no topo da pilha
+constant op_ADD      : std_logic_vector(3 DOWNTO 0):="1000"; --Desempilha Op1 e Op2 e empilha (Op1 + Op2).
+constant op_SUB      : std_logic_vector(3 DOWNTO 0):="1001"; --Desempilha Op1 e Op2 e empilha (Op1 − Op2).
+constant op_NAND     : std_logic_vector(3 DOWNTO 0):="1010"; --Desempilha Op1 e Op2 e empilha NAND(Op1, Op2).
+constant op_SLT      : std_logic_vector(3 DOWNTO 0):="1011"; --Desempilha Op1 e Op2 e empilha (Op1 < Op2).
+constant op_SHL      : std_logic_vector(3 DOWNTO 0):="1100"; --Desempilha Op1 e Op2 e empilha (Op1 ≪ Op2).
+constant op_SHR      : std_logic_vector(3 DOWNTO 0):="1101"; --Desempilha Op1 e Op2 e empilha (Op1 ≫ Op2).
+constant op_JEQ      : std_logic_vector(3 DOWNTO 0):="1110"; --Desempilha Op1(1 byte), Op2(1 byte) e Op3(2 bytes); Verifica se (Op1 = Op2), caso positivo soma Op3 no registrador IP.
+constant op_JMP      : std_logic_vector(3 DOWNTO 0):="1111"; --Desempilha Op1(2 bytes) e o atribui no registrador IP
 
-signal IP :std_logic_vector(addr_width) := "0000000000000000";
-signal SP :std_logic_vector(data_width) := "0000000000000000";
+signal IP :std_logic_vector(addr_width-1 downto 0) := "0000000000000000";
+signal SP :std_logic_vector(addr_width-1 downto 0) := "0000000000000000";
 
-signal instruction : in std_logic_vector(addr_width-1 downto 0);
-signal opcode : in std_logic_vector(4);
+--signal instruction : in std_logic_vector(addr_width-1 downto 0);
+signal opcode : std_logic_vector(3 downto 0);
 
 begin
 
-decode : process(clock)
-        begin
-            if rising_edge(clk) then
-                opcode <= instruction_in(addr_width-1 downto addr_width-4);
-            end if;
-end decode;
+    execute : process(clock)
+    begin
+        if rising_edge(clock) then
+            --Manda valor do registrador IP pra IMEM
+            instruction_addr <= IP; 
+            --Pega opcode da instrucao
+            opcode <= instruction_in(addr_width-1 downto addr_width-4); 
+            --Executa opcode
+                case opcode is
+                    when op_HLT => 
 
+                    when op_IN => 
 
-execute : process()
-            begin
+                    when op_OUT => 
 
-                    case opcode is
-                        when HLT
+                    when op_PUSHIP => 
 
-                        WHEN IN_                
-                        halt <= '1';
-                        read_signal <= '1';
-                        write_signal <= '0';
-                        --fazer pulso--
-                        interrupt <=
+                    when op_PUSH_IMM => 
 
-                        when OUT_
+                    when op_DROP => 
 
-                        when PUSHIP 
+                    when op_DUP => 
 
-                        when PUSH_IMM
+                    when op_ADD => 
 
-                        when DROP
+                    when op_SUB => 
 
-                        when DUP
+                    when op_NAND => 
 
-                        when ADD 
-                        -- recebe da mem--
-                        op1 <= data_in(addr)
-                        op2  <= data_in(addr+1)
+                    when op_SLT => 
 
-                        -- desempilha da DMEM--
-                        drop(addr)
-                        drop(addr+1)
+                    when op_SHR => 
 
-                        --opera--
-                        aux <= std_logic_vector(to_signed( (to_integer(signed(A)) + to_integer(signed(B)) ),N) );
-                        --empilha--
-                        mem_data_out(addr) <= aux;
+                    when op_JEQ => 
 
-                        when SUB
-                         -- recebe da mem--
-                         op1 <= data_in(addr)
-                         op  <= data_in(addr+1)
- 
-                         -- desempilha da DMEM--
-                         drop(addr)
-                         drop(addr+1)
- 
-                         --opera--
-                         aux <= std_logic_vector(to_signed( (to_integer(signed(A)) - to_integer(signed(B)) ),N) )
-                         --empilha--
-                         mem_data_out(addr) <= aux;
+                    when op_JMP => 
 
-                        when NAND_
-
-                        when SLT 
-
-                        when SHR
-
-                        when JEQ
-
-                        when JMP 
-        end execute;
-
-Write_back :process()
-                begin
-
-
-
-
-
-                end Write_back;
-
-end;
+                    when others =>
+                end case;
+        end if;
+    end process;
+end architecture;
 
 
 -- lembrar passos, fetch , decode, execute, write-back(buscar instrucoes, decodificar, executar, escrever na memoria resultado)
